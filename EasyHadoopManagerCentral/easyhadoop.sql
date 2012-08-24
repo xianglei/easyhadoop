fbe0a71c770cd5ebdda571cb9f4a383ed04a1bcc
xianglei <horseman@163.com>
2012-08-25 02:41:02 +0800

    add initialized sql file

---
+++ EasyHadoopManagerCentral/easyhadoop.sql
@@ -0,0 +1,109 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.2.2
+-- http://www.phpmyadmin.net
+--
+-- 主机: localhost
+-- 生成日期: 2012 年 08 月 25 日 02:40
+-- 服务器版本: 5.5.24-0ubuntu0.12.04.1
+-- PHP 版本: 5.3.10-1ubuntu3.2
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- 数据库: `easyhadoop`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `ehm_hadoop_settings`
+--
+
+DROP TABLE IF EXISTS `ehm_hadoop_settings`;
+CREATE TABLE IF NOT EXISTS `ehm_hadoop_settings` (
+  `set_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `value` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `filename` varchar(255) NOT NULL,
+  PRIMARY KEY (`set_id`),
+  KEY `filename` (`filename`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `ehm_hive_settings`
+--
+
+DROP TABLE IF EXISTS `ehm_hive_settings`;
+CREATE TABLE IF NOT EXISTS `ehm_hive_settings` (
+  `set_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `value` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `filename` varchar(255) NOT NULL,
+  PRIMARY KEY (`set_id`),
+  KEY `filename` (`filename`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `ehm_hosts`
+--
+
+DROP TABLE IF EXISTS `ehm_hosts`;
+CREATE TABLE IF NOT EXISTS `ehm_hosts` (
+  `host_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `hostname` varchar(255) NOT NULL,
+  `ip` varchar(255) NOT NULL,
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`host_id`),
+  KEY `hostname` (`hostname`,`ip`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `ehm_host_settings`
+--
+
+DROP TABLE IF EXISTS `ehm_host_settings`;
+CREATE TABLE IF NOT EXISTS `ehm_host_settings` (
+  `set_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `filename` varchar(255) NOT NULL,
+  `content` mediumint(9) NOT NULL,
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `host_id` int(10) NOT NULL,
+  PRIMARY KEY (`set_id`),
+  KEY `filename` (`filename`,`host_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Setting files of each host' AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `ehm_pig_settings`
+--
+
+DROP TABLE IF EXISTS `ehm_pig_settings`;
+CREATE TABLE IF NOT EXISTS `ehm_pig_settings` (
+  `set_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `value` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `filename` varchar(255) NOT NULL,
+  PRIMARY KEY (`set_id`),
+  KEY `filename` (`filename`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
