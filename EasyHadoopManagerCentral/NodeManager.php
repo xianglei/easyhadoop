e40eb885fbc652aa40d037977d541671b3462700
xianglei <horseman@163.com>
2012-08-27 18:20:42 +0800

    test

--- EasyHadoopManagerCentral/NodeManager.php
+++ EasyHadoopManagerCentral/NodeManager.php
@@ -9,14 +9,38 @@ $mysql = new Mysql();
 if(!@$_GET['action'])
 {
 	echo '<div class=span10>';
+	
 	$sql = "select * from ehm_hosts order by create_time desc";
 	$mysql->Query($sql);
+	echo '<table class="table table-striped">';
+	echo '<thead>
+                <tr>
+                  <th>#</th>
+                  <th>'.$lang['hostname'].'</th>
+                  <th>'.$lang['ipAddr'].'</th>
+                  <th>'.$lang['nodeRole'].'</th>
+                  <th>'.$lang['createTime'].'</th>
+                </tr>
+                </thead>
+                <tbody>';
+	$i = 0;
 	while($arr = $mysql->FetchArray())
 	{
+		echo '<tr>
+                  <td>'.$i.'</td>
+                  <td>'.$arr['hostname'].'</td>
+                  <td>'.$arr['ip'].'</td>
+                  <td>'.$arr['role'].'</td>
+                  <td>'.$arr['create_time'].'</td>
+                </tr>';
 		echo $arr["hostname"]."</br>";
+		$i++;
 	}
+	echo '</tbody></table>';
 	echo '</div>';
 }
+
+
 elseif ($_GET['action'] == "AddNode")
 {
 	if(!$_POST['ip'] && !$_POST['hostname'] && !$_POST['role'])
