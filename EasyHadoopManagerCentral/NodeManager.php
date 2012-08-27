d5a768e912e64c40d5411c25fd53a291831d220c
xianglei <horseman@163.com>
2012-08-27 17:58:21 +0800

    test

--- EasyHadoopManagerCentral/NodeManager.php
+++ EasyHadoopManagerCentral/NodeManager.php
@@ -8,22 +8,22 @@ $mysql = new Mysql();
 
 if(!@$_GET['action'])
 {
+	echo '<div class=span3>';
 	$sql = "select * from ehm_hosts order by create_time desc";
 	$mysql->Query($sql);
 	while($arr = $mysql->FetchArray())
 	{
 		echo $arr["hostname"]."</br>";
 	}
+	echo '</div>';
 }
 elseif ($_GET['action'] == "AddNode")
 {
 	if(!$_POST['ip'] && !$_POST['hostname'] && !$_POST['role'])
 	{
-		echo '<div class="page-header">
+		echo '<div class="span3">
             <h1>'.$lang['addNode'].'</h1>
-        </div>
-        
-		<form method=POST>
+            <form method=POST>
 				<label>'.$lang['hostname'].'</label><br />
 				<input type="text" placeholder="'.$lang['hostname'].'" name="hostname" /><br />
 				<label>'.$lang['ipAddr'].'</label><br />
@@ -32,16 +32,19 @@ elseif ($_GET['action'] == "AddNode")
 				<input type="text" placeholder="'.$lang['roleName'].'" name="role" /><br />
 				<input type="hidden" name="action" value="'.$_GET['action'].'" />
 				<button type="submit" class="btn">'.$lang['submit'].'</button>
-		</form>';
+		</form>
+		</div>';
 	}
 	else
 	{
+		echo '<div class=span3>';
 		$hostname = $_POST['hostname'];
 		$ipaddr = $_POST['ipaddr'];
 		$role = $_POST['role'];
 		$sql = "insert ehm_hosts set hostname = '".$hostname."', ip = '".$ipaddr."', role = '".$role."', create_time=current_timestamp()";
 		echo $sql;
 		$mysql->Query($sql);
+		echo '</div>';
 	}
 }
 
