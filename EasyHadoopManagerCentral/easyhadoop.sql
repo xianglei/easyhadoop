b92146c4823e28ca4c7456e44549a26c1ba03434
xianglei <horseman@163.com>
2012-08-30 14:42:02 +0800

    new sql

--- EasyHadoopManagerCentral/easyhadoop.sql
+++ EasyHadoopManagerCentral/easyhadoop.sql
@@ -1,21 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 3.5.2.2
--- http://www.phpmyadmin.net
---
--- 主机: localhost
--- 生成日期: 2012 年 08 月 25 日 02:40
--- 服务器版本: 5.5.24-0ubuntu0.12.04.1
--- PHP 版本: 5.3.10-1ubuntu3.2
-
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
 --
 -- 数据库: `easyhadoop`
 --
@@ -28,12 +10,12 @@ SET time_zone = "+00:00";
 
 DROP TABLE IF EXISTS `ehm_hadoop_settings`;
 CREATE TABLE IF NOT EXISTS `ehm_hadoop_settings` (
-  `set_id` int(11) NOT NULL AUTO_INCREMENT,
+  `set_id` int(11) NOT NULL auto_increment,
   `name` varchar(255) NOT NULL,
   `value` varchar(255) NOT NULL,
   `description` text NOT NULL,
   `filename` varchar(255) NOT NULL,
-  PRIMARY KEY (`set_id`),
+  PRIMARY KEY  (`set_id`),
   KEY `filename` (`filename`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
@@ -45,12 +27,12 @@ CREATE TABLE IF NOT EXISTS `ehm_hadoop_settings` (
 
 DROP TABLE IF EXISTS `ehm_hive_settings`;
 CREATE TABLE IF NOT EXISTS `ehm_hive_settings` (
-  `set_id` int(11) NOT NULL AUTO_INCREMENT,
+  `set_id` int(11) NOT NULL auto_increment,
   `name` varchar(255) NOT NULL,
   `value` varchar(255) NOT NULL,
   `description` text NOT NULL,
   `filename` varchar(255) NOT NULL,
-  PRIMARY KEY (`set_id`),
+  PRIMARY KEY  (`set_id`),
   KEY `filename` (`filename`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
@@ -62,14 +44,15 @@ CREATE TABLE IF NOT EXISTS `ehm_hive_settings` (
 
 DROP TABLE IF EXISTS `ehm_hosts`;
 CREATE TABLE IF NOT EXISTS `ehm_hosts` (
-  `host_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `host_id` int(11) unsigned NOT NULL auto_increment,
   `hostname` varchar(255) NOT NULL,
   `ip` varchar(255) NOT NULL,
   `role` varchar(255) NOT NULL,
-  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`host_id`),
+  `create_time` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+  PRIMARY KEY  (`host_id`),
+  UNIQUE KEY `hostname_2` (`hostname`),
   KEY `hostname` (`hostname`,`ip`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=7 ;
 
 -- --------------------------------------------------------
 
@@ -79,14 +62,15 @@ CREATE TABLE IF NOT EXISTS `ehm_hosts` (
 
 DROP TABLE IF EXISTS `ehm_host_settings`;
 CREATE TABLE IF NOT EXISTS `ehm_host_settings` (
-  `set_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `set_id` int(10) unsigned NOT NULL auto_increment,
   `filename` varchar(255) NOT NULL,
-  `content` mediumint(9) NOT NULL,
-  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `host_id` int(10) NOT NULL,
-  PRIMARY KEY (`set_id`),
-  KEY `filename` (`filename`,`host_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Setting files of each host' AUTO_INCREMENT=1 ;
+  `content` mediumtext NOT NULL,
+  `create_time` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+  `ip` varchar(16) NOT NULL,
+  PRIMARY KEY  (`set_id`),
+  UNIQUE KEY `filename_2` (`filename`),
+  KEY `filename` (`filename`,`ip`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='Setting files of each host' AUTO_INCREMENT=9 ;
 
 -- --------------------------------------------------------
 
@@ -96,15 +80,11 @@ CREATE TABLE IF NOT EXISTS `ehm_host_settings` (
 
 DROP TABLE IF EXISTS `ehm_pig_settings`;
 CREATE TABLE IF NOT EXISTS `ehm_pig_settings` (
-  `set_id` int(11) NOT NULL AUTO_INCREMENT,
+  `set_id` int(11) NOT NULL auto_increment,
   `name` varchar(255) NOT NULL,
   `value` varchar(255) NOT NULL,
   `description` text NOT NULL,
   `filename` varchar(255) NOT NULL,
-  PRIMARY KEY (`set_id`),
+  PRIMARY KEY  (`set_id`),
   KEY `filename` (`filename`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
