431b497ea1d04d7ee88e8f54fcec78bf06cd4b56
xianglei <horseman@163.com>
2012-08-31 17:53:45 +0800

    debug extends

--- EasyHadoopManagerCentral/classes/class.user.php
+++ EasyHadoopManagerCentral/classes/class.user.php
@@ -5,8 +5,8 @@ class User extends Mysql
 	public function AuthUser($pUsername, $pPassword)
 	{
 		$sql = "select count(*) from ehm_user where username = '".$pUsername."', password = '".md5($pPassword)."'";
-		$mysql->Query($sql);
-		$arr = $mysql->FetchArray();
+		$this->Query($sql);
+		$arr = $this->FetchArray();
 		if($arr[0] == 0)
 		{
 			return FALSE;
@@ -22,7 +22,7 @@ class User extends Mysql
 		if($this->AuthUser($pUsername, $pPassword))
 		{
 			$sql = "update ehm_user set password = '".md5($pNewPassword)."' where username = '".$pUsername."'";
-			$mysql->Query($sql);
+			$this->Query($sql);
 			return TRUE;
 		}
 		else
