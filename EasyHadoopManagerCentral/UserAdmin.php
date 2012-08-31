6d198c0383845403c2c2ec5960b8c3f59896c0f1
xianglei <horseman@163.com>
2012-08-31 20:19:18 +0800

    debug

--- EasyHadoopManagerCentral/UserAdmin.php
+++ EasyHadoopManagerCentral/UserAdmin.php
@@ -12,50 +12,55 @@ if(!$_GET['action'])
 elseif($_GET['action'] == "ChangePassword")
 {
 	echo '<div class=span10>';
-	
-	if(!$_POST['cur_pass'] || !$_POST['new_pass'])
+	if(!$_POST['action'])
 	{
-		include_once "templates/change_password.html";		
+		include_once "templates/change_password.html";
 	}
 	else
 	{
-		echo "<h3>";
-		
-		if($auth->AuthUser($_SESSION['username'],$_POST['cur_pass']))
+		if(!$_POST['cur_pass'] || !$_POST['new_pass'])
 		{
-			if($_POST['cur_pass'] == $_POST['new_pass1'])
-			{
-				echo $lang['passwordEqual'];
-			}
-			else
-			{
-				if($_POST['new_pass1'] != $_POST['new_pass2'])
-				{
-					echo $lang['passwordNotEqual'];
-				}
-				else
-				{
-					$username = $_SESSION['username'];
-					$password = $_POST['password'];
-					$newpassword = $_POST['new_pass1'];
-					if($auth->ChangePassword($username, $password, $newpassword))
-					{
-						echo $lang['changePasswordSuccess'];
-					}
-					else
-					{
-						echo $lang['changePasswordFailed'];
-					}
-				}
-			}
+			include_once "templates/change_password.html";
 		}
 		else
 		{
-			echo $lang['notValidPassword'];
+			echo "<h3>";
+
+			if($auth->AuthUser($_SESSION['username'],$_POST['cur_pass']))
+			{
+				if($_POST['cur_pass'] == $_POST['new_pass1'])
+				{
+					echo $lang['passwordEqual'];
+				}
+				else
+				{
+					if($_POST['new_pass1'] != $_POST['new_pass2'])
+					{
+						echo $lang['passwordNotEqual'];
+					}
+					else
+					{
+						$username = $_SESSION['username'];
+						$password = $_POST['password'];
+						$newpassword = $_POST['new_pass1'];
+						if($auth->ChangePassword($username, $password, $newpassword))
+						{
+							echo $lang['changePasswordSuccess'];
+						}
+						else
+						{
+							echo $lang['changePasswordFailed'];
+						}
+					}
+				}
+			}
+			else
+			{
+				echo $lang['notValidPassword'];
+			}
+			echo "</h3>";
 		}
-		echo "</h3>";
 	}
-	
 	echo '</div>';
 }
 
