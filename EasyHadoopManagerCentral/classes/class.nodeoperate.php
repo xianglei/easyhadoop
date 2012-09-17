ead5494adc86668a8acec6ed6199b31f2dc98872
xianglei <horseman@163.com>
2012-09-17 17:48:27 +0800

    debug

--- EasyHadoopManagerCentral/classes/class.nodeoperate.php
+++ EasyHadoopManagerCentral/classes/class.nodeoperate.php
@@ -111,7 +111,7 @@ class NodeOperator extends Socket
 			$str = $lang['notConnected'];
 		}
 		$str = str_replace('ERROR', "<b><font color=red>ERROR</font></b>",$str);
-		$str = str_replace('WARN', "<b><font color=orange>ERROR</font></b>",$str);
+		$str = str_replace('WARN', "<b><font color=orange>WARN</font></b>",$str);
 		return $str;
 	}
 	
