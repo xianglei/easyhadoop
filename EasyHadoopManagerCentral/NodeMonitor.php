452a1e8aa0551b25816d159fae186c8986e2e2a6
xianglei <horseman@163.com>
2012-10-11 16:28:28 +0800

    debug

--- EasyHadoopManagerCentral/NodeMonitor.php
+++ EasyHadoopManagerCentral/NodeMonitor.php
@@ -131,7 +131,7 @@ elseif($_GET['action'] == "NodeMrUsed")
 	
 	echo "<pre>";
 	
-	$fp = fopen("http://".$ip.":50030/tasktracker.jsp","r");
+	$fp = fopen("http://".$ip.":50060/tasktracker.jsp","r");
 	while(!feof($fp))
 	{
 		echo $str = fread($fp,4096);
