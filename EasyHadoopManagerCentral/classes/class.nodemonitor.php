cb957132087beebf9cb22c27f748633df3dac41b
xianglei <horseman@163.com>
2012-11-28 20:48:17 +0800

    Remove GetJson function

--- EasyHadoopManagerCentral/classes/class.nodemonitor.php
+++ EasyHadoopManagerCentral/classes/class.nodemonitor.php
@@ -59,43 +59,6 @@ class NodeMonitor
 		return $json;
 	}
 	
-	public function GetJson($pHost,$pRole)
-	{
-		switch ($pRole)
-		{
-			case 'namenode':
-				$port = "50070";
-				break;
-			case 'datanode':
-				$port = '50075';
-				break;
-			case 'jobtracker':
-				$port = '50030';
-				break;
-			case 'tasktracker':
-				$port = '50060';
-				break;
-			case 'secondarynamenode':
-				$port = '50090';
-				break;
-			
-			default:
-				return FALSE;
-				break;
-		}
-		$url = "http://".$pHost.":".$port."/jmx";
-		$fp = @fopen($url, "r");
-		while(!@feof($fp))
-		{
-			$json .= @fread($fp,1024);
-		}
-		@fclose($fp);
-		
-		$arr = $this->ParseJson($json);
-		
-		return $arr;
-	}
-	
 	public function ParseJson($pJson)
 	{
 		$arr = json_decode($pJson);
