0549b421fc4d58d8294261c176247f5b0434e379
chuan <photoshopcn@gmail.com>
2012-12-26 17:19:57 +0800

    update navbar css
    add active class

--- EasyHadoopCentral/application/views/nav_bar.php
+++ EasyHadoopCentral/application/views/nav_bar.php
@@ -4,22 +4,22 @@
           <a class="brand" href="<?php echo $this->config->base_url();?>"><?php echo $common_title;?></a>
           <div class="nav-collapse collapse">
             <ul class="nav">
-              <li>
+              <li <?php if($this->router->class == "index"){ echo "class=\"active\"";}?>>
                 <a href="<?php echo $this->config->base_url();?>"><?php echo $common_index_page;?></a>
               </li>
-              <li class="">
+              <li <?php if($this->router->class == "settings"){ echo "class=\"active\"";}?>>
                 <a href="<?php echo $this->config->base_url();?>index.php/settings/index/"><?php echo $common_host_settings?></a>
               </li>
-              <li class="">
+              <li <?php if($this->router->class == "install"){ echo "class=\"active\"";}?>>
                 <a href="<?php echo $this->config->base_url();?>index.php/install/index/" ><?php echo $common_install;?></a>
               </li>
-              <li>
+              <li <?php if($this->router->class == "operate"){ echo "class=\"active\"";}?>>
                 <a href="<?php echo $this->config->base_url();?>index.php/operate/index/"><?php echo $common_node_operate;?></a>
               </li>
-              <li class="">
+              <li <?php if($this->router->class == "monitor"){ echo "class=\"active\"";}?>>
                 <a href="<?php echo $this->config->base_url();?>index.php/monitor/index/"><?php echo $common_node_monitor;?></a>
               </li>
-              <li class="">
+              <li <?php if($this->router->class == "user"){ echo "class=\"active\"";}?>>
                 <a href="<?php echo $this->config->base_url();?>index.php/user/updatepassword/"><?php echo $common_user_admin?></a>
               </li>
 			  <!--<li class="">
