4e8cc9c2e3acf4cf866fc5e0036d42ef1ac7c704
xianglei <horseman@163.com>
2013-01-16 19:21:43 +0800

    debug

--- EasyHadoopCentral/application/views/ehm_host_monitor_load_list.php
+++ EasyHadoopCentral/application/views/ehm_host_monitor_load_list.php
@@ -3,9 +3,10 @@
 	<pre>
 	Sample:<br />
 		<div class="progress">
-			<div class="bar bar-info" style="width: 33%;">1 min LoadAvg</div>
-			<div class="bar bar-warning" style="width: 33%;">5 min LoadAvg</div>
-			<div class="bar bar-danger" style="width: 34%;">15 min LoadAvg</div>
+			<div class="bar bar-info" style="width: 25%;">1 min LoadAvg</div>
+			<div class="bar bar-warning" style="width: 25%;">5 min LoadAvg</div>
+			<div class="bar bar-danger" style="width: 25%;">15 min LoadAvg</div>
+			<div class="bar bar-danger" style="width: 25%;">Free</div>
 		</div>
 	</pre>
 	
@@ -29,6 +30,7 @@
 				<div class="bar bar-info" id="load1_<?php echo $item->host_id;?>" style="">1</div>
 				<div class="bar bar-warning" id="load5_<?php echo $item->host_id;?>" style="">5</div>
 				<div class="bar bar-danger" id="load15_<?php echo $item->host_id;?>" style="">15</div>
+				<div class="bar bar-danger" id="free_<?php echo $item->host_id;?>" style="">15</div>
 			</div>
 			<script>
 			function host_load_<?php echo $item->host_id;?>()
@@ -40,10 +42,12 @@
 					total = load1 + load5 + load15;
 					load1_per = Math.round(load1/total);
 					load5_per = Math.round(load5/total);
-					load15_per = 100 - load1_per - load5_per;
+					load15_per = Math.round(load15/total);
+					free_per = 100 - load1_per - load5_per;
 					$('#load1_<?php echo $item->host_id;?>').attr('style', 'width: ' + load1_per + '%;');
 					$('#load5_<?php echo $item->host_id;?>').attr('style', 'width: ' + load5_per + '%;');
 					$('#load15_<?php echo $item->host_id;?>').attr('style', 'width: ' + load15_per + '%;');
+					$('#free_<?php echo $item->host_id;?>').attr('style', 'width: ' + free_per + '%;');
 				});
 			}
 			host_load_<?php echo $item->host_id;?>();
