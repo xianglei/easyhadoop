0bd6c2765283876760f3b3579f9f7e1ca36b5993
foxmoon <smile93@sina.com>
2013-03-22 17:31:32 +0800

    加入进度条，加入删除提示，优化删除速度,加入根目录显示

---
+++ EasyHadoopCentral/js/bootstrap-confirm.js
@@ -0,0 +1,30 @@
+﻿(function ($) {
+    $.fn.extend({
+        //pass the options variable to the function
+        confirmModal: function (options) {
+            var html = '<div class="modal" id="confirmContainer"><div class="modal-header"><a class="close" data-dismiss="modal">×</a>' +
+            '<h3>#Heading#</h3></div><div class="modal-body">' +
+            '#Body#</div><div class="modal-footer">' +
+            '<a href="#" class="btn btn-primary" id="confirmYesBtn">Confirm</a>' +
+            '<a href="#" class="btn" data-dismiss="modal">Close</a></div></div>';
+
+            var defaults = {
+                heading: 'Please confirm',
+                body:'Body contents',
+                callback : null
+            };
+            
+            var options = $.extend(defaults, options);
+            html = html.replace('#Heading#',options.heading).replace('#Body#',options.body);
+            $(this).html(html);
+            $(this).modal('show');
+            var context = $(this); 
+            $('#confirmYesBtn',this).click(function(){
+                if(options.callback!=null)
+                    options.callback();
+                $(context).modal('hide');
+            });
+        }
+    });
+
+})(jQuery);
