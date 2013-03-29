70856ba96d2eaed18a743895f0a3f3f15666c2c6
foxmoon <smile93@sina.com>
2013-03-29 16:01:36 +0800

    加入监控失败列表功能

--- EasyHadoopCentral/application/controllers/operate.php
+++ EasyHadoopCentral/application/controllers/operate.php
@@ -77,6 +77,71 @@ class Operate extends CI_Controller
 		#generaet footer
 		$this->load->view('footer', $data);		
 	}
+	public function ErrorPage()
+	{
+		#Generate header
+		$this->lang->load('commons');
+		$data['common_lang_set'] = $this->lang->line('common_lang_set');
+		$data['common_title'] = $this->lang->line('common_title');
+		$this->load->view('header',$data);
+		
+		#generate navigation bar
+		$data['common_index_page'] = $this->lang->line('common_index_page');
+		$data['common_node_manager'] = $this->lang->line('common_node_manager');
+		$data['common_node_monitor'] = $this->lang->line('common_node_monitor');
+		$data['common_install'] = $this->lang->line('common_install');
+		$data['common_host_settings'] = $this->lang->line('common_host_settings');
+		$data['common_node_operate'] = $this->lang->line('common_node_operate');
+		$data['common_user_admin'] = $this->lang->line('common_user_admin');
+		$data['common_log_out'] = $this->lang->line('common_log_out');
+		$data['common_hadoop_node_operate'] = $this->lang->line('common_hadoop_node_operate');
+		$data['common_hbase_node_operate'] = $this->lang->line('common_hbase_node_operate');
+		$data['common_hadoop_host_settings'] = $this->lang->line('common_hadoop_host_settings');
+		$data['common_hbase_host_settings'] = $this->lang->line('common_hbase_host_settings');
+		$data['common_install_hadoop'] = $this->lang->line('common_install_hadoop');
+		$data['common_install_hbase'] = $this->lang->line('common_install_hbase');
+		$this->load->view('nav_bar', $data);
+		
+		$this->load->view('div_fluid');
+		$this->load->view('div_row_fluid');
+		
+		$this->load->view('ehm_hosts_operate_nav', $data);
+		
+		$data['common_hostname'] = $this->lang->line('common_hostname');
+		$data['common_ip_addr'] = $this->lang->line('common_ip_addr');
+		$data['common_node_role'] = $this->lang->line('common_node_role');
+		$data['common_create_time'] = $this->lang->line('common_create_time');
+		$data['common_action'] = $this->lang->line('common_action');
+		
+		#generate pagination
+		$this->load->model('ehm_hosts_model', 'hosts');
+		$this->load->library('pagination');
+		$config['base_url'] = $this->config->base_url() . 'index.php/operate/index/';
+		$config['total_rows'] = $this->hosts->count_hosts($this->input->get('q',""));
+		$config['per_page'] = 20;
+		if($this->input->get('q',"")!="")
+		$config['suffix'] = "?q=".$this->input->get('q',"");
+		
+		$offset = $this->uri->segment(3,0);
+		if($offset == 0):
+			$offset = 0;
+		else:
+			$offset = ($offset / $config['per_page']) * $config['per_page'];
+		endif;
+		$this->pagination->initialize($config);
+		$data['pagination'] = $this->pagination->create_links();
+		
+		$data['results'] = $this->hosts->get_hosts_list($config['per_page'], $offset,$this->input->get('q',""));
+		$data['q']=$this->input->get('q',"");
+		
+		$this->load->view('ehm_hosts_error_list',$data);
+		
+		$this->load->view('div_end');
+		$this->load->view('div_end');
+		
+		#generaet footer
+		$this->load->view('footer', $data);		
+	}	
 	
 	public function ViewLogs()
 	{
