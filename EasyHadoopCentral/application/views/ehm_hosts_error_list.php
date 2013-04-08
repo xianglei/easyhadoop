a75ab1a9bc23a6109456850d76eae0e82f1d1557
foxmoon <smile93@sina.com>
2013-04-08 10:48:17 +0800

    加入jmx状态监视

--- EasyHadoopCentral/application/views/ehm_hosts_error_list.php
+++ EasyHadoopCentral/application/views/ehm_hosts_error_list.php
@@ -8,29 +8,36 @@ function viewlogs(role, host_id)
 		$('#viewlogs_'+role+'_'+host_id).html('<small>'+html+'</small>');
 	});
 }
-function check_online(role, host_id)
+function check_online()
 {
 	$.ajaxSettings.async = false;
-	$.getJSON('<?php echo $this->config->base_url();?>index.php/monitor/getpid/'+host_id+'/' + role, function(json){
-		if(json.status == 'online')
+	$.getJSON('<?php echo $this->config->base_url();?>index.php/monitor/getallstats/', function(json){
+		
+		var deads=eval(json);
+	
+		for(var i=0;i<deads.length;i++)
 		{
-			$('#status_'+ role +'_'+host_id).removeClass("btn");
-			$('#status_'+ role +'_'+host_id).addClass("btn btn-success");
-			//$('#show_node_'+host_id).css("display","none");
-		}
-		else
-		{
-			$('#status_'+role+'_'+host_id).removeClass("btn");
-			$('#status_'+role+'_'+host_id).addClass("btn btn-danger");
-			$('#show_node_'+host_id).css("display","block");
+			
+			$('#show_node_'+deads[i].replace(".","_")).css("display","block");
+			
+			
+		
 		}
 	});
 }
+
 function autoSubmit(val)
 {
 	if(val && (val.length>0))
 	this.location="<?php echo $this->config->base_url();?>index.php/operate/index/?q="+val;
 
+}
+function refresh_fail_list()
+{
+$("tr[id^='show_node_']").each(function() {
+               $(this).css("display","none");    
+                });
+check_online();				
 }
 </script>
 
@@ -47,66 +54,24 @@ function autoSubmit(val)
 	</thead>-->
 	<tbody>
 <?php $i = 1; foreach($results as $item):?>
-		<tr  id="show_node_<?php echo $item->host_id;?>" style="display:none">
+		<tr  id="show_node_<?php echo str_replace(".","_",$item->hostname);?>" style="display:none">
 			<td><?php echo $i;?></td>
 			<td><?php echo $item->hostname;?>
 			</td>
 			<td><?php echo $item->ip;?>
 			</td>
 			<td>
-			<table border="0" class="table-condensed" >
-			<thead>
-				<tr>
-					
-<?php
-$tmp = explode(",", $item->role);
-foreach($tmp as $k => $v):
-?>
-					<th>
-				<div class="btn-group"  >
-					<a class="btn" id="status_<?php echo $v;?>_<?php echo $item->host_id;?>" data-toggle="modal"  href="#view_<?php echo $v?>_<?php echo $item->host_id?>_modal"><?php echo $v;?> </a>
-					<button class="btn dropdown-toggle" data-toggle="dropdown"><span class="caret"></span></button>
-					<ul class="dropdown-menu">
-						<li><a data-toggle="modal" href="#view_<?php echo $v?>_<?php echo $item->host_id?>_modal"><i class="icon-search"></i><?php echo $common_view_logs;?></a></li>
-						<li class="divider"></li>
-						<li><a href="<?php echo $this->config->base_url();?>index.php/operate/start/<?php echo $item->host_id?>/<?php echo $v?>"><i class="icon-play"></i><?php echo $common_start;?> <?php echo $v;?></a></li>
-						<li><a href="<?php echo $this->config->base_url();?>index.php/operate/stop/<?php echo $item->host_id?>/<?php echo $v?>"><i class="icon-stop"></i><?php echo $common_stop;?> <?php echo $v;?></a></li>
-						<li><a href="<?php echo $this->config->base_url();?>index.php/operate/restart/<?php echo $item->host_id?>/<?php echo $v?>"><i class="icon-refresh"></i><?php echo $common_restart;?> <?php echo $v;?></a></li>
-					</ul>
-				</div>
-				</th>
-				<!--modaled view logs hdd usage-->
-					<div class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" id="view_<?php echo $v?>_<?php echo $item->host_id?>_modal">
-						<div class="modal-header">
-							<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-							<h4><?php echo $item->hostname;?> : <?php echo $item->ip;?> : <?php echo $v;?></h4>
-						</div>
-						<div class="modal-body" id="viewlogs_<?php echo $v?>_<?php echo $item->host_id?>">
-						</div>
-						<div class="modal-footer">
-							<button class="btn" data-dismiss="modal" aria-hidden="true"><?php echo $common_close;?></button>
-						</div>
-					</div>
-				<!--modal end-->
 
-				<script>
-			
-				check_online('<?php echo $v?>', <?php echo $item->host_id?>);
-				
-				</script>
-			
-<?php
-endforeach;
-?>
-					</tr>
-				</thead>
-				</table>
+
 			</td>
 		</tr>
 <?php $i++; endforeach;?>
 	</tbody>
 </table>
-
+<script>
+			
+				check_online();
+</script>
 <div>
 		<h3></h3>
 	</div>
---
+++ EasyHadoopCentral/application/views/ehm_refresh_fail_operate_nav.php
@@ -0,0 +1,6 @@
+<div class="span2">
+	<div class="btn-group btn-group-vertical">
+			<a href="javascript:void(0)"  onclick ='refresh_fail_list();' class="btn btn-info"><i class="icon-thumbs-up"></i><?php echo $common_hadoop_refresh_fail;?></a>
+			
+	</div>
+</div>
